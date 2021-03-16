0ca41af5803df476061b462746ce78f580133ffd
Grzegorz Daniluk <gdaniluk@gmail.com>
2021-03-16 12:46:24 +0100

    Improvments.

--- keyboards/bastardkb/tbk3/keymaps/gregd/keymap.c
+++ keyboards/bastardkb/tbk3/keymaps/gregd/keymap.c
@@ -9,14 +9,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         OSM(MOD_LALT), KC_Q, KC_W, KC_E, KC_R, KC_T, /**/ KC_Y, KC_U, KC_I, KC_O, KC_P, KC_EQL, \
         OSM(MOD_LSFT), KC_A, KC_S, KC_D, KC_F, KC_G, /**/ KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, \
         OSM(MOD_LCTL), KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RCTL), \
-        LT(_IDE,KC_LGUI), LT(_NAV,KC_BSPC), LT(_SYMBOLS,KC_ESC), /**/ LT(_SYMBOLS,KC_ENT), LT(_NAV,KC_SPC), LT(_IDE,KC_TAB)),
+        LT(_IDE, KC_LGUI), LT(_NAV, KC_BSPC), LT(_SYMBOLS, KC_ESC), /**/ LT(_SYMBOLS, KC_ENT), LT(_NAV, KC_SPC), LT(_IDE, KC_TAB)),
 
 	[_POLISH] = LAYOUT_split_4x6_3( \
-        GD_QWERTY, RALT(KC_A), RALT(KC_S), RALT(KC_E), RALT(KC_Z), RALT(KC_C), /**/ RALT(KC_X), RALT(KC_N), RALT(KC_L), RALT(KC_O), _______, _______, \
+        GD_QWERTY, RALT(KC_X), RALT(KC_S), RALT(KC_E), RALT(KC_A), RALT(KC_C), /**/ RALT(KC_Z), RALT(KC_N), RALT(KC_L), RALT(KC_O), _______, _______, \
         _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______, \
-        _______, _______, _______, MT(MOD_RALT, KC_D), _______, _______, /**/ _______, _______, MT(MOD_RALT, KC_K), _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______, \
-        KC_RALT, _______, KC_RALT, /**/ KC_RALT, _______, KC_RALT),
+        _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______, \
+        _______, _______, MT(MOD_RALT, KC_ESC), /**/ MT(MOD_RALT, KC_ENT), _______, _______),
 
 	[_NAV] = LAYOUT_split_4x6_3( \
         GD_QWERTY, KC_TILD,  KC_CIRC,  KC_PERC, GD_ARROW, GD_FARROW, /**/ KC_NO,              BABL_DEL_TO_LINE_START, BABL_DEL_TO_LINE_END, KC_NO,             KC_NO,       KC_NO, \
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         BABL_WINDOW_NEXT, BABL_CLOSE_APP,       BABL_IDE_FILE_FIND, BABL_BR_PREV_TAB, BABL_FIND_NEXT, BABL_IDE_QUICK_DOC, /**/ KC_1,  KC_2,  KC_3,  KC_4,  KC_5,   KC_F11, \
         BABL_BR_ZOOM_IN,  GD_ALT_TAB,           BABL_BR_URL_BAR,    BABL_BR_NEXT_TAB, BABL_FIND,      BABL_UNDO,          /**/ KC_6,  KC_7,  KC_8,  KC_9,  KC_0,   KC_F12, \
         BABL_BR_ZOOM_OUT, BABL_SWITCH_APP_NEXT, BABL_BR_CLOSE_TAB,  BABL_BR_NEW_TAB,  BABL_FIND_PREV, BABL_REDO,          /**/ KC_MINS, GD_ARROW, GD_FARROW, GD_ABRACKET, KC_TILDE, _______, \
-        KC_LEAD, _______, _______, /**/ _______, _______, _______),
+        _______, _______, KC_LEAD, /**/ _______, _______, _______),
 
 	[_IDE] = LAYOUT_split_4x6_3( \
         RESET, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT, KC_MPRV, /**/ KC_CALC, KC_MAIL, KC_MSEL, KC_MYCM, KC_NO, GD_INFO, \
