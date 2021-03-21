39a3d31b2c749420af361c72042d2cfbbd718e27
Grzegorz Daniluk <gdaniluk@gmail.com>
2021-03-21 17:49:51 +0100

    Remove first row changes.

--- keyboards/bastardkb/tbk3/keymaps/gregd/keymap.c
+++ keyboards/bastardkb/tbk3/keymaps/gregd/keymap.c
@@ -6,16 +6,16 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_4x6_3( \
     /*             pinky       ring     middle   index                                                      index         middle       ring     pinky */
-    KC_LEAD,       KC_EXCLAIM, KC_AT,   KC_HASH, KC_DOLLAR, KC_PERCENT,                 /**/ KC_UNDERSCORE, KC_AMPERSAND, KC_ASTERISK, KC_MINS, KC_PLUS,  KC_CIRCUMFLEX, \
-    OSM(MOD_LALT), KC_Q,       KC_W,    KC_E,    KC_R,      KC_T,                       /**/ KC_Y,          KC_U,         KC_I,        KC_O,    KC_P,     KC_PEQL, \
+    KC_LEAD,       KC_1,       KC_2,    KC_3,    KC_4,      KC_5,                       /**/ KC_6,          KC_7,         KC_8,        KC_9,    KC_0,     GD_RESET, \
+    OSM(MOD_RALT), KC_Q,       KC_W,    KC_E,    KC_R,      KC_T,                       /**/ KC_Y,          KC_U,         KC_I,        KC_O,    KC_P,     KC_PEQL, \
     OSM(MOD_LSFT), KC_A,       KC_S,    KC_D,    KC_F,      KC_G,                       /**/ KC_H,          KC_J,         KC_K,        KC_L,    KC_SCLN,  KC_QUOT, \
     OSM(MOD_LCTL), KC_Z,       KC_X,    KC_C,    KC_V,      KC_B,                       /**/ KC_N,          KC_M,         KC_COMM,     KC_DOT,  KC_SLSH,  OSM(MOD_RCTL), \
     \
     LT(_IDE, KC_LGUI), LT(_NAV, KC_BSPC), LT(_SYMBOLS, KC_ESC),                         /**/ LT(_SYMBOLS, KC_ENT), LT(_NAV, KC_SPC), LT(_IDE, KC_TAB)),
 
   [_POLISH] = LAYOUT_split_4x6_3( \
-    /*      pinky       ring        middle      index                                                    index       middle      ring        pinky */
-    GD_QWE, RALT(KC_X), RALT(KC_S), RALT(KC_E), RALT(KC_A), RALT(KC_C),                 /**/ RALT(KC_Z), RALT(KC_N), RALT(KC_L), RALT(KC_O), _______, _______, \
+    /*       pinky    ring     middle   index                                                         index    middle   ring     pinky */
+    GD_QWE,  _______, _______, _______, _______, _______,                               /**/ _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______,                               /**/ _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______,                               /**/ _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______,                               /**/ _______, _______, _______, _______, _______, _______, \
