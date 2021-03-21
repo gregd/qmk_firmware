3b3169ddf5d9e5b9cc3069cff42b146342880fa3
Grzegorz Daniluk <gdaniluk@gmail.com>
2021-03-21 18:53:13 +0100

    Fixes

--- keyboards/bastardkb/tbk3/keymaps/gregd/keymap.c
+++ keyboards/bastardkb/tbk3/keymaps/gregd/keymap.c
@@ -6,12 +6,12 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_4x6_3( \
     /*             pinky       ring     middle   index                                                      index         middle       ring     pinky */
-    KC_LEAD,       KC_1,       KC_2,    KC_3,    KC_4,      KC_5,                       /**/ KC_6,          KC_7,         KC_8,        KC_9,    KC_0,     GD_RESET, \
+    KC_LGUI,       KC_1,       KC_2,    KC_3,    KC_4,      KC_5,                       /**/ KC_6,          KC_7,         KC_8,        KC_9,    KC_0,     GD_RESET, \
     OSM(MOD_RALT), KC_Q,       KC_W,    KC_E,    KC_R,      KC_T,                       /**/ KC_Y,          KC_U,         KC_I,        KC_O,    KC_P,     KC_PEQL, \
     OSM(MOD_LSFT), KC_A,       KC_S,    KC_D,    KC_F,      KC_G,                       /**/ KC_H,          KC_J,         KC_K,        KC_L,    KC_SCLN,  KC_QUOT, \
     OSM(MOD_LCTL), KC_Z,       KC_X,    KC_C,    KC_V,      KC_B,                       /**/ KC_N,          KC_M,         KC_COMM,     KC_DOT,  KC_SLSH,  OSM(MOD_RCTL), \
     \
-    LT(_IDE, KC_LGUI), LT(_NAV, KC_BSPC), LT(_SYMBOLS, KC_ESC),                         /**/ LT(_SYMBOLS, KC_ENT), LT(_NAV, KC_SPC), LT(_IDE, KC_TAB)),
+    LT(_IDE, KC_ESC), LT(_NAV, KC_BSPC), LT(_SYMBOLS, KC_DEL),                          /**/ LT(_SYMBOLS, KC_ENT), LT(_NAV, KC_SPC), LT(_IDE, KC_TAB)),
 
   [_POLISH] = LAYOUT_split_4x6_3( \
     /*       pinky    ring     middle   index                                                         index    middle   ring     pinky */
@@ -29,14 +29,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_AT,     KC_ASTR,  KC_UNDS,  KC_LPRN, KC_RPRN,  KC_BSLASH,                        /**/ KC_LEFT,     KC_DOWN,     KC_UP,       KC_RIGHT,      GD_COPY,     GD_I_EX_SEL, \
     KC_HASH,   KC_AMPR,  KC_PLUS,  KC_LBRC, KC_RBRC,  KC_PIPE,                          /**/ GD_GO_SDOC,  KC_PGDN,     KC_PGUP,     GD_GO_EDOC,    GD_CUT,      GD_SELECT_ALL, \
     \
-    KC_APP, GD_DEL_LWORD, KC_DEL,                                                       /**/ _______, _______, _______),
+    KC_APP, GD_DEL_LWORD, GD_DEL_RWORD,                                                 /**/ _______, _______, _______),
 
   [_SYMBOLS] = LAYOUT_split_4x6_3( \
-    /*            pinky         ring           middle         index                                      index      middle       ring       pinky */
-    GD_QWE,       KC_F1,        KC_F2,         KC_F3,         KC_F4,     KC_F5,         /**/ KC_F6,      KC_F7,     KC_F8,       KC_F9,     GD_SCR_CAP_SEL, GD_SCR_CAP_FULL, \
-    GD_APP_SAVE,  GD_WIN_NEXT,  GD_B_URL_BAR,  GD_B_NEW_TAB,  GD_FIND,   GD_REPLACE,    /**/ KC_1,       KC_2,      KC_3,        KC_4,      KC_5,           KC_F10, \
-    GD_WIN_CEN,   GD_APP_NEXT,  GD_ALT_TAB,    GD_B_NEXT_TAB, GD_FNEXT,  GD_UNDO,       /**/ KC_6,       KC_7,      KC_8,        KC_9,      KC_0,           KC_F11, \
-    GD_WIN_HIDE,  GD_CLOSE_APP, GD_B_CL_TAB,   GD_B_PREV_TAB, GD_FPREV,  GD_REDO,       /**/ GD_FARROW,  GD_ARROW,  GD_ABRACKET, GD_HDIR,   GD_UDIR,        KC_F12, \
+    /*            pinky         ring           middle         index                                      index      middle      ring      pinky */
+    GD_QWE,       KC_F1,        KC_F2,         KC_F3,         KC_F4,     KC_F5,         /**/ KC_F6,      KC_F7,     KC_F8,      KC_F9,    GD_SCR_CAP_SEL, GD_SCR_CAP_FULL, \
+    GD_APP_SAVE,  GD_WIN_NEXT,  GD_B_URL_BAR,  GD_B_NEW_TAB,  GD_FIND,   GD_REPLACE,    /**/ GD_FARROW,  KC_7,      KC_8,       KC_9,     GD_HDIR,        KC_F10, \
+    GD_WIN_CEN,   GD_APP_NEXT,  GD_ALT_TAB,    GD_B_NEXT_TAB, GD_FNEXT,  GD_UNDO,       /**/ GD_ARROW,   KC_4,      KC_5,       KC_6,     GD_ABRACKET,    KC_F11, \
+    GD_WIN_HIDE,  GD_CLOSE_APP, GD_B_CL_TAB,   GD_B_PREV_TAB, GD_FPREV,  GD_REDO,       /**/ KC_0,       KC_1,      KC_2,       KC_3,     GD_UDIR,        KC_F12, \
     \
     _______, _______, KC_LEAD,                                                          /**/ GD_I_NEW_LINE, GD_I_COMPLETE, GD_I_FIXES),
 
