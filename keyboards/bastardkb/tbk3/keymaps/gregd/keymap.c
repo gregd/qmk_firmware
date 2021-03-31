c8a945cf2e57fff8701a127a6cebf0eed994608d
Grzegorz Daniluk <gdaniluk@gmail.com>
2021-03-31 23:18:30 +0200

    Fixes

--- keyboards/bastardkb/tbk3/keymaps/gregd/keymap.c
+++ keyboards/bastardkb/tbk3/keymaps/gregd/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*                     pinky  ring   middle index                                                      index         middle       ring     pinky */
     OSM(MOD_LALT),         KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                           /**/ KC_6,          KC_7,         KC_8,        KC_9,    KC_0,     KC_NO, \
     OSM(MOD_LGUI),         KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                           /**/ KC_Y,          KC_U,         KC_I,        KC_O,    KC_P,     KC_CAPS, \
-    OSM(MOD_LSFT),         KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                           /**/ KC_H,          KC_J,         KC_K,        KC_L,    KC_SCLN,  OSM(MOD_LSFT), \
+    OSM(MOD_LSFT),         KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                           /**/ KC_H,          KC_J,         KC_K,        KC_L,    KC_SCLN,  KC_QUOT, \
     OSM(MOD_LCTL),         KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                           /**/ KC_N,          KC_M,         KC_COMM,     KC_DOT,  KC_SLSH,  OSM(MOD_RCTL), \
     \
     LT(_IDE_LEFT, KC_DEL), LT(_SYMBOLS, KC_ENT), LT(_NAV, KC_ESC),                     /**/ LT(_NAV, KC_TAB), LT(_SYMBOLS, KC_SPC), LT(_IDE_RIGHT, KC_BSPC)),
