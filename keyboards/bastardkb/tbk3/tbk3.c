cbf6824406b5fff93b7e76a0cf13ef0d66552c01
Grzegorz Daniluk <gdaniluk@gmail.com>
2021-04-03 18:48:41 +0200

    Fixes

--- keyboards/bastardkb/tbk3/tbk3.c
+++ keyboards/bastardkb/tbk3/tbk3.c
@@ -258,7 +258,7 @@ void eeconfig_init_user(void) {
 }
 
 void keyboard_post_init_user(void) {
-    debug_enable = true;
+    //debug_enable = true;
     user_config.raw = eeconfig_read_user();
     // By default Linux mode is set so set the new mode only in case of Mac mode
     if (user_config.mac_mode) {
